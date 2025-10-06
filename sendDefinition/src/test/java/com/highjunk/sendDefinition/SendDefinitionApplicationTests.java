c324165489f43927e190afb93904443b19881b8f
sekiyak <k.sekiya.highjunk@gmail.com>
2025-10-06 17:43:34 +0900

    新規ファイル

--- sendDefinition/src/test/java/com/highjunk/sendDefinition/SendDefinitionApplicationTests.java
+++ sendDefinition/src/test/java/com/highjunk/sendDefinition/SendDefinitionApplicationTests.java
@@ -1,5 +1,7 @@
 package com.highjunk.sendDefinition;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class SendDefinitionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("まだ実装されていません");
 	}
 
 }
